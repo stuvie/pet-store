82c63403094f0b19dd1c39fe912bf5266d77e34f
Steve Kotsopoulos <sk@fywss.com>
2018-04-05 20:59:50 -0400

    add freshly-generated pet-services folder, which will be the spring boot backend

---
+++ pet-services/src/main/java/com/fywss/spring/petservices/PetServicesApplication.java
@@ -0,0 +1,12 @@
+package com.fywss.spring.petservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetServicesApplication.class, args);
+	}
+}
