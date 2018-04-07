58a2f183d3d0a73741fd7c0179bd7ceef37e6785
Steve Kotsopoulos <sk@fywss.com>
2018-04-07 18:01:22 -0400

    update pet urls

--- pet-services/src/main/resources/data.sql
+++ pet-services/src/main/resources/data.sql
@@ -1,12 +1,12 @@
-insert into pet (name, category, status, photo_url) values ('Ziggy',	'cat', 'pet', 'https://i.imgur.com/uQ0TA26.jpg');
-insert into pet (name, category, status, photo_url) values ('Dexter',	'cat', 'pet', 'https://i.imgur.com/D5YY8cl.jpg');
-insert into pet (name, category, status, photo_url) values ('Kenickie',	'cat', 'pet', 'https://i.imgur.com/LOuSgXH.jpg');
+insert into pet (name, category, status, photo_url) values ('Ziggy',	'cat', 'sold', 'https://i.imgur.com/uQ0TA26b.jpg');
+insert into pet (name, category, status, photo_url) values ('Dexter',	'cat', 'available', 'https://i.imgur.com/D5YY8clb.jpg');
+insert into pet (name, category, status, photo_url) values ('Kenickie',	'cat', 'on hold', 'https://i.imgur.com/LOuSgXHb.jpg');
 
-insert into pet (name, category, status, photo_url) values ('Coconut',	'dog', 'available', 'https://i.imgur.com/RKtCwG8.jpg');
-insert into pet (name, category, status, photo_url) values ('Sleepy',	'dog', 'available', 'https://i.imgur.com/OHGV0Gt.jpg');
-insert into pet (name, category, status, photo_url) values ('Piper',	'dog', 'available', 'https://i.imgur.com/0rTegcY.jpg');
-insert into pet (name, category, status, photo_url) values ('BossHog',	'dog', 'available', 'https://i.imgur.com/cmK4snf.jpg');
-insert into pet (name, category, status, photo_url) values ('Karl',		'dog', 'available', 'https://i.imgur.com/A3mG7xY.jpg');
-insert into pet (name, category, status, photo_url) values ('Bear',		'dog', 'available', 'https://i.imgur.com/gM37Bi4.jpg');
-insert into pet (name, category, status, photo_url) values ('Chico',	'dog', 'available', 'https://i.imgur.com/oGK5hGL.jpg');
-insert into pet (name, category, status, photo_url) values ('Henry',	'dog', 'available', 'https://i.imgur.com/iidzlDY.jpg');
+insert into pet (name, category, status, photo_url) values ('Coconut',	'dog', 'sold', 'https://i.imgur.com/RKtCwG8b.jpg');
+insert into pet (name, category, status, photo_url) values ('Sleepy',	'dog', 'available', 'https://i.imgur.com/OHGV0Gtb.jpg');
+insert into pet (name, category, status, photo_url) values ('Piper',	'dog', 'available', 'https://i.imgur.com/0rTegcYb.jpg');
+insert into pet (name, category, status, photo_url) values ('BossHog',	'dog', 'available', 'https://i.imgur.com/cmK4snfb.jpg');
+insert into pet (name, category, status, photo_url) values ('Karl',		'dog', 'available', 'https://i.imgur.com/A3mG7xYb.jpg');
+insert into pet (name, category, status, photo_url) values ('Bear',		'dog', 'available', 'https://i.imgur.com/gM37Bi4b.jpg');
+insert into pet (name, category, status, photo_url) values ('Chico',	'dog', 'available', 'https://i.imgur.com/oGK5hGLb.jpg');
+insert into pet (name, category, status, photo_url) values ('Henry',	'dog', 'available', 'https://i.imgur.com/iidzlDYb.jpg');
