b993968a2f519d1d7b65416c80b57696ea96c4e4
Steve Kotsopoulos <sk@fywss.com>
2018-04-07 09:26:13 -0400

    add more pets and fix the photoUrl

--- pet-services/src/main/resources/data.sql
+++ pet-services/src/main/resources/data.sql
@@ -1,4 +1,12 @@
-insert into pet (name, category, status, photo_url) values ('Ziggy',   'cat', 'pet', 'https://imgur.com/uQ0TA26');
-insert into pet (name, category, status, photo_url) values ('Coconut', 'dog', 'available', 'https://imgur.com/RKtCwG8');
-insert into pet (name, category, status, photo_url) values ('Sleepy', 'dog', 'available', 'https://imgur.com/OHGV0Gt');
-insert into pet (name, category, status, photo_url) values ('Piper',   'dog', 'taken', 'https://imgur.com/0rTegcY');
+insert into pet (name, category, status, photo_url) values ('Ziggy',	'cat', 'pet', 'https://i.imgur.com/uQ0TA26.jpg');
+insert into pet (name, category, status, photo_url) values ('Dexter',	'cat', 'pet', 'https://i.imgur.com/D5YY8cl.jpg');
+insert into pet (name, category, status, photo_url) values ('Kenickie',	'cat', 'pet', 'https://i.imgur.com/LOuSgXH.jpg');
+
+insert into pet (name, category, status, photo_url) values ('Coconut',	'dog', 'available', 'https://i.imgur.com/RKtCwG8.jpg');
+insert into pet (name, category, status, photo_url) values ('Sleepy',	'dog', 'available', 'https://i.imgur.com/OHGV0Gt.jpg');
+insert into pet (name, category, status, photo_url) values ('Piper',	'dog', 'available', 'https://i.imgur.com/0rTegcY.jpg');
+insert into pet (name, category, status, photo_url) values ('BossHog',	'dog', 'available', 'https://i.imgur.com/cmK4snf.jpg');
+insert into pet (name, category, status, photo_url) values ('Karl',		'dog', 'available', 'https://i.imgur.com/A3mG7xY.jpg');
+insert into pet (name, category, status, photo_url) values ('Bear',		'dog', 'available', 'https://i.imgur.com/gM37Bi4.jpg');
+insert into pet (name, category, status, photo_url) values ('Chico',	'dog', 'available', 'https://i.imgur.com/oGK5hGL.jpg');
+insert into pet (name, category, status, photo_url) values ('Henry',	'dog', 'available', 'https://i.imgur.com/iidzlDY.jpg');
